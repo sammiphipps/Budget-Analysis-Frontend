7fc16cf84a27cd9482024ef5451912fbee891ca1
Samantha Phipps <sammi.phipps@gmail.com>
2020-07-09 18:27:36 -0400

    Started to setup App.js. Removed logo from src file, as I will not need it. Reset App.css. Added state and fetched categories, budgets and transactions.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    categories: [],
+    budgets: [],
+    transactions: []
+  }
+
+  componentDidMount(){
+    Promise.all([
+      fetch('http://localhost:3000/categories'),
+      fetch('http://localhost:3000/budgets'),
+      fetch('http://localhost:3000/transactions')
+    ]).then(responses => Promise.all(responses.map(response => response.json())))
+    .then(finalVals => {
+      this.setState({categories: [...finalVals[0].data]})
+      this.setState({budgets: [...finalVals[1].data]})
+      this.setState({transactions: [...finalVals[2].data]})
+    }).catch(errors => console.log(errors))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>Budget Tracker</h1>
+        </header>
+        <main>
+
+        </main>
+      </div>
+    )    
+  }
 }
 
 export default App;
