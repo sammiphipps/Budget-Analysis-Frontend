94efd30989c88a69911d7779ad778964f934a49a
Samantha Phipps <sammi.phipps@gmail.com>
2020-07-11 11:22:06 -0400

    Successfuly created function that will find the category totals for the month. In order to do this, I has to created multiple functions that would find the data needed.

--- src/components/AnalysisHomePage/AnalysisHomePage.js
+++ src/components/AnalysisHomePage/AnalysisHomePage.js
@@ -2,13 +2,18 @@ import React, {Component} from 'react';
 import './AnalysisHomePage.css';
 
 export default class AnalysisHomePage extends Component {
-    findThreeLargestCategoryAmounts = (transactions, categoryId) => {
-        const categoryTransactions = transactions.filter(transaction => {
+
+    parseAmount = (amount) => {
+        return parseFloat(amount.replace(/(^\$|,)/g,''))
+    }
+
+    findThreeLargestCategoryAmounts = (categoryId) => {
+        const categoryTransactions = this.props.transactions.filter(transaction => {
             return transaction.relationships.category.data.id === categoryId
         })
 
         const categoryTransactionSortedByAmount = categoryTransactions.sort((a,b) => {
-            return parseFloat(b.attributes.amount.replace(/(^\$|,)/g,'')) - parseFloat(a.attributes.amount.replace(/(^\$|,)/g,''))
+            return this.parseAmount(b.attributes.amount) - this.parseAmount(a.attributes.amount)
         })
         
         if(categoryTransactionSortedByAmount.length > 3) {
@@ -18,8 +23,8 @@ export default class AnalysisHomePage extends Component {
         }
     }
 
-    showThreeLargestCategoryAmounts = (transactions, categoryId) => {
-        const largestItems = this.findThreeLargestCategoryAmounts(transactions, categoryId)
+    showThreeLargestCategoryAmounts = (categoryId) => {
+        const largestItems = this.findThreeLargestCategoryAmounts(categoryId)
         return <table>
             <tbody>
             {
@@ -34,12 +39,66 @@ export default class AnalysisHomePage extends Component {
         </table>
     }
 
+    findCategoryTotalsForMonth = (month) => {
+        const groupMonthTransationsByCategory = this.findTransactionsForMonthByCategory(month)
+        const categoryTransactionTotals = Object.keys(groupMonthTransationsByCategory).map( category => {
+            const categoryTransactions = groupMonthTransationsByCategory[category]
+            const categoryTotal = categoryTransactions.reduce((total, transaction) => {
+                return total += this.parseAmount(transaction.attributes.amount)
+            }, 0)  
+            return categoryTotal
+        })
+        return Object.keys(groupMonthTransationsByCategory).map((category, index) => {
+            const categoryHash = {}
+            categoryHash["category"] = category
+            categoryHash["totalAmountSpent"] = categoryTransactionTotals[index]
+            return categoryHash
+        })
+    }
+
+    findTransactionsForMonthByCategory = (month) => {        
+        const monthTransactions = this.filterTransactionsByMonth(month)
+        return this.groupByCategory(monthTransactions)
+    }
+
+    filterTransactionsByMonth = (month) => {
+        return this.props.transactions.filter(transaction => {
+            const splitTransactionDate = transaction.attributes.date.split('-')
+            const transactionDate = new Date(splitTransactionDate[0], splitTransactionDate[1] - 1, splitTransactionDate[2])
+            return (transactionDate.getMonth() + 1) === month
+        })
+    }
+
+    groupByCategory = (array) => {
+        return  array.reduce((result, element) => {
+            const categoryName = this.findCategoryName(element.relationships.category.data.id)
+            if (!result[categoryName]) {
+                result[categoryName] = [];
+            }
+            result[categoryName].push(element);
+            return result
+        }, {})
+    }
+
+
+    findCategoryName = (categoryId) => {
+        const filteredCategory = this.props.categories.filter(category => category.id === categoryId)
+        return filteredCategory[0].attributes.name
+    }
+
     render(){
         const {categories, transactions} = this.props
         return (
             <div id="AnalysisHomePage">
                 <div id="graphs">
-                    <div id="lineGraph">Budget vs Spent for the year will go here</div>
+                    <div id="lineGraph">
+                        <p>Budget vs Spent for the year will go here</p>
+                        {
+                            transactions.length !== 0
+                                ? console.log(this.findCategoryTotalsForMonth(6))
+                                : ''
+                        }
+                    </div>
                     <div id="pieChart">Pie Chart detailing how much was spent for each category for the month will go here.</div>
                 </div>
                 <div id="categorySpending">
@@ -49,7 +108,7 @@ export default class AnalysisHomePage extends Component {
                         }).map(category => {
                             return <div key={category.id} id={category.attributes.name}>
                                 <h3>{category.attributes.name}</h3>
-                                {this.showThreeLargestCategoryAmounts(transactions, category.id)}
+                                {this.showThreeLargestCategoryAmounts(category.id)}
                             </div>
                         })
                     }
